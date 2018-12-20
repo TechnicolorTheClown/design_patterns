b678272d310b2cd0c5d41f3e8304bf6ed6e71f30
环境变量 <1421718760@qq.com>
2018-12-20 09:41:42 +0800

    单例模式

--- src/main/java/com/design/patterns_1_单例模式/TMain_demo.java
+++ src/main/java/com/design/patterns_1_单例模式/TMain_demo.java
@@ -1,5 +1,10 @@
 package com.design.patterns_1_单例模式;
 
+
+/**
+ * 由结果可以得知单例模式为一个面向对象的应用程序提供了对象惟一的访问点，
+ * 不管它实现何种功能，整个应用程序都会同享一个实例对象
+ */
 public class TMain_demo {
     public static void main(String[] args){
         TestSingleton ts1 = TestSingleton.getInstance();
