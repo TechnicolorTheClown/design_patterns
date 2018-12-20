d9cd7e263dae14e51ba5b564f421ece9be1a87f8
环境变量 <1421718760@qq.com>
2018-12-20 09:36:20 +0800

    单例模式

--- src/main/java/com/design/patterns_1_单例模式/TestSingleton.java
+++ src/main/java/com/design/patterns_1_单例模式/TestSingleton.java
@@ -1,4 +1,39 @@
 package com.design.patterns_1_单例模式;
 
+
+/**
+ * 以懒汉式为例，
+ * 这里为了保证线程安全，
+ * 使用了双重检查锁定的方式：
+ */
 public class TestSingleton {
+    String name = null;
+
+    private TestSingleton() {
+    }
+
+    private static volatile TestSingleton instance = null;
+
+    public static TestSingleton getInstance() {
+        if (instance == null) {
+            synchronized (TestSingleton.class) {
+                if (instance == null) {
+                    instance = new TestSingleton();
+                }
+            }
+        }
+        return instance;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void printInfo() {
+        System.out.println("the name is " + name);
+    }
 }
